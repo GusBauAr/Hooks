37eabb7f5f3a4d71b5bb5dd4934a74d6ee4e87ef
GusBauAr <gustavo.bautista.arce.2000@gmail.com>
2025-05-25 23:32:44 -0400

    useState practice mostrar y ocultar

--- src/UseState.jsx
+++ src/UseState.jsx
@@ -1,19 +1,24 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function UseState() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const [visible, setVisible] = useState(true)
 
   return (
     <>
-      
-      <h1>useState:</h1>
+      <h1>useState Contador</h1>
       <h1>{count}</h1>
-        <button onClick={() => setCount(count + 1)}>
-          click
-        </button>
-      
+      <button onClick={() => setCount(count + 1)}>click</button>
+
+      <h1>useState mostrar y ocultar</h1>
+      { visible && <p>Gustavo Bautista Arce</p>}
+      <button onClick={() => setVisible(!visible)}>
+        {visible ? "Ocultar" : "Mostrar"}
+      </button>
+
     </>
-  )
+  );
 }
-export default UseState
+export default UseState;
